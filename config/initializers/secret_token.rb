ecba5b9c2f34604283713de0a809c977f991bfbb
App Academy Student <student@appacademy.io>
2014-06-25 09:25:23 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpLite::Application.config.secret_key_base = '466c03f81383bb7ef3babd9d312d88caa0ef93fe645c2a76eff472ca1a22b2768f7db935b8269e39a5768110722e50f49c795f4251cdd55722842af93e81d36f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YelpLite::Application.config.session_store :cookie_store, key: '_YelpLite_session'

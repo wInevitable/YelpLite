53c79fddec7a10f5fc630a02f68be6c9bb01945f
Will Noll <williampnoll@gmail.com>
2014-06-25 09:42:05 -0400

    Devise Install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ YelpLite::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'yelplite.heroku.com' }
 end
